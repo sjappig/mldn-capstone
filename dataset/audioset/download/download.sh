658f445423c4038c35bd80bbcb47bc52a366bb2d
Jari-Pekka Ryynänen <jaripekka.ryynanen@ge.com>
2017-06-25 14:19:45 +0300

    Add download script for getting raw audio
    The original script is from unixpickle/audioset.
    Local changes:
     * fs 16 kHz
     * mono,
     * filename prefixed with sample_
     * no gzip

---
+++ dataset/audioset/download/download.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SAMPLE_RATE=16000
+
+# fetch_clip(videoID, startTime, endTime)
+fetch_clip() {
+  echo "Fetching $1 ($2 to $3)..."
+  outname="sample_$1_$2"
+  if [ -f "${outname}.wav" ]; then
+    echo "Already have it."
+    return
+  fi
+
+  youtube-dl https://youtube.com/watch?v=$1 \
+    --quiet --extract-audio --audio-format wav \
+    --output "$outname.%(ext)s"
+  if [ $? -eq 0 ]; then
+    # If we don't pipe `yes`, ffmpeg seems to steal a
+    # character from stdin. I have no idea why.
+    yes | ffmpeg -loglevel quiet -i "./$outname.wav" -ar $SAMPLE_RATE \
+      -ac 1 -ss "$2" -to "$3" "./${outname}_out.wav"
+    mv "./${outname}_out.wav" "./$outname.wav"
+  else
+    # Give the user a chance to Ctrl+C.
+    sleep 1
+  fi
+}
+
+grep -E '^[^#]' | while read line
+do
+  fetch_clip $(echo "$line" | sed -E 's/, / /g')
+done
