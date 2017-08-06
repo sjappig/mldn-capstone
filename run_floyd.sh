15f384dcfa1a0247164852be335b4de6a764f8e4
Jari-Pekka Ryynänen <jaripekka.ryynanen@ge.com>
2017-08-06 14:05:48 +0300

    Update run_floyd-script

--- run_floyd.sh
+++ run_floyd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-floyd run --gpu --env tensorflow-1.2:py2 --no-open --data sjappig/audioset_train/1:pp_data "python -m audiolabel.fit_and_predict /pp_data/train.h5 --N 1000 --skip baseline"
+floyd run --gpu --env tensorflow-1.2:py2 --no-open --data sjappig/audioset_train/2:pp_data "python -m audiolabel.fit_and_predict /pp_data/train.h5 --test /pp_data/test.h5 --skip baseline --epochs 1500 --validation-size 0"
 
